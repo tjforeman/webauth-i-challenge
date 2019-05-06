2c4b163b15b0c1d246b5c13bd791b10d8a4c4b26
tjforeman <tylerforeman92@gmail.com>
2019-05-06 16:07:13 -0400

    register and login routes working correctly

--- routes/login-route.js
+++ routes/login-route.js
@@ -1,6 +1,25 @@
 const knex=require('knex');
 const router = require('express').Router();
 const db =require('../data/dbConfig.js');
+const bcrypt=require('bcryptjs')
+
+router.post('/', (req, res) => {
+    let {username,password}=req.body 
+    db('users')
+    .where({username})
+    .first()
+    .then(user =>{
+        if(user&& bcrypt.compareSync(password,user.password)){
+        res.status(200).json({message: `Welcome ${user.username}, you have successfully logged in `})
+        }else{
+            res.status(401).json({message:'you shall not pass!'})
+        }
+        })
+    .catch(err=>{
+        res.status(500).json({error:err,message:'Unable to log in at this time' })
+    })
+      });
 
 
 module.exports=router
+
